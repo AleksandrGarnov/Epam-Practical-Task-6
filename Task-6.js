eb211f5b3a872d20c5d0b6651deba54e353fc53b
Aleksandr Garnov <sgarnov97@gmail.com>
2022-07-20 14:52:28 +0400

    Modified Changes

--- Task-6.js
+++ Task-6.js
@@ -3,11 +3,14 @@ const firsStr = 'ahb acb aeb aeeb adcd axeb';
 const matchStr = firsStr.match(/a.b/gi);
 
 console.log(matchStr);
+
 // 2.Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest //
 const secondStr = '2 + 3 223 2223';
 const regExp = /2 \+\ 3/;
-const result = secondStr.match(regExp)
+const result = secondStr.match(regExp);
+
 console.log(result);
+
 // 3.Get the day, month and year of the current date and output it to the console separately //
  const now = new Date();;
  const nowDay = now.getDate();
